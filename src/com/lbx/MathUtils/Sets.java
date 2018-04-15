a3e36e4580044facad864342cdb762af68a1c3d7
xxx <837944603@qq.com>
2018-04-15 08:30:58 +0800

    数学工具类

---
+++ src/com/lbx/MathUtils/Sets.java
@@ -0,0 +1,64 @@
+//: net/mindview/util/Sets.java
+package com.lbx.MathUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sets {
+    /**
+     * 求并集
+     *
+     * @param a
+     * @param b
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<T>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    /**
+     * 交集
+     *
+     * @param a
+     * @param b
+     * @param <T>
+     * @return
+     */
+    public static <T>
+    Set<T> intersection(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<T>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+
+    /**
+     * 求不包含右边的元素
+     *
+     * @param superset
+     * @param subset
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T>
+    difference(Set<T> superset, Set<T> subset) {
+        Set<T> result = new HashSet<T>(superset);
+        result.removeAll(subset);
+        return result;
+    }
+
+
+    /**
+     * 求交集之外的东西
+     * @param a
+     * @param b
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> complement(Set<T> a, Set<T> b) {
+        return difference(union(a, b), intersection(a, b));
+    }
+} ///:~
