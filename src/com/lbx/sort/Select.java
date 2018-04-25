b616d19ec6ce7ce506b81ce460beebe179b72006
lbx <837944603@qq.com>
2018-04-25 10:14:46 +0800

    选择排序

--- src/com/lbx/sort/Select.java
+++ src/com/lbx/sort/Select.java
@@ -9,11 +9,11 @@ public class Select {
     public static void main(String[] args) {
         int[] arr = {5, 6, 8, 1, 88, 66, 7, 8, 20};
         for (int i = 0; i < arr.length; i++) {
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
