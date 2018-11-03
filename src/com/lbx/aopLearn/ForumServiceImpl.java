6386da036c5ac229d89d7a6214c618522e427789
xxx <837944603@qq.com>
2018-11-03 15:01:14 +0800

    AOP学习

---
+++ src/com/lbx/aopLearn/ForumServiceImpl.java
@@ -0,0 +1,31 @@
+package com.lbx.aopLearn;
+
+/**
+ * Create by lbx on 2018/11/3  14:47
+ **/
+public class ForumServiceImpl implements ForumService {
+    @Override
+    public void removeTopic(int topicId) {
+        PerformanceMonitor.begin("com.lbx.aopLearn.ForumServiceImpl.removeTopic");
+        System.out.println("模拟删除Topic记录"+topicId);
+        try {
+            Thread.currentThread().sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        PerformanceMonitor.end();
+    }
+
+    @Override
+    public void removeForum(int forumId) {
+        PerformanceMonitor.begin("com.lbx.aopLearn.ForumServiceImpl.removeForum");
+        System.out.println("模拟删除Forum记录"+forumId);
+        try {
+            Thread.currentThread().sleep(40);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        PerformanceMonitor.end();
+    }
+
+}
