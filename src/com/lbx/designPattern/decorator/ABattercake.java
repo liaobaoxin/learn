e5eba83482d09a09c0c84fbf91a6b29958f1b06c
Administrator <837944603@qq.com>
2019-11-06 11:46:23 +0800

    装饰者模式

---
+++ src/com/lbx/designPattern/decorator/ABattercake.java
@@ -0,0 +1,6 @@
+package com.lbx.designPattern.decorator;
+
+public abstract  class ABattercake {
+    protected abstract String getDesc();
+    protected abstract int cost();
+}
---
+++ src/com/lbx/designPattern/decorator/Battercake.java
@@ -0,0 +1,13 @@
+package com.lbx.designPattern.decorator;
+
+public class Battercake extends ABattercake {
+    @Override
+    protected String getDesc() {
+        return "煎饼";
+    }
+
+    @Override
+    protected int cost() {
+        return 8;
+    }
+}
